6513dba76e673e38db061645b06c82ed62365b38
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-01-21 19:52:22 +0300

    Add Client model specs

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:client)).to be_valid
+  end
+
+  let(:subject) { build(:client) }
+
+  describe 'ActiveModel validations' do
+    include_examples :person_active_model_validations
+  end
+
+  describe 'ActiveRecord associations' do
+    # Associations
+    it { expect(subject).to have_many(:estate).dependent(:destroy) }
+
+    include_examples :person_active_record_associations
+  end
 end
