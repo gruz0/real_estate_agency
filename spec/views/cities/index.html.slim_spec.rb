53b9a723f930a4ce3a7854d1e169693528306acc
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-01-30 01:21:49 +0300

    Add link to streets with number of streets to City#index view

--- spec/views/cities/index.html.slim_spec.rb
+++ spec/views/cities/index.html.slim_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe 'cities/index', type: :view do
   it 'renders a list of cities' do
     assign(:cities, cities)
 
+    3.times { |_| create(:street, city: city1) }
+    4.times { |_| create(:street, city: city2) }
+
     render
 
     assert_select 'h1', text: I18n.t('views.city.index.title'), count: 1
@@ -18,6 +21,7 @@ RSpec.describe 'cities/index', type: :view do
       assert_select 'thead' do
         assert_select 'th', text: City.human_attribute_name(:id), count: 1
         assert_select 'th', text: City.human_attribute_name(:name), count: 1
+        assert_select 'th', text: City.human_attribute_name(:number_of_streets), count: 1
         assert_select 'th', text: City.human_attribute_name(:created_at), count: 1
         assert_select 'th', text: City.human_attribute_name(:updated_at), count: 1
       end
@@ -26,6 +30,8 @@ RSpec.describe 'cities/index', type: :view do
         assert_select 'tr', count: 2
         assert_select 'tr td', text: 'Нефтеюганск', count: 1
         assert_select 'tr td', text: 'Сургут', count: 1
+        assert_select 'tr td a[href=?]', city_streets_path(city1), count: 1, text: '3'
+        assert_select 'tr td a[href=?]', city_streets_path(city2), count: 1, text: '4'
         assert_select 'tr td', text: city1.created_at.to_s, count: 4
       end
     end
