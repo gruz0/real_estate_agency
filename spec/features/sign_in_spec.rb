3238bfa88c95bdd27682f4bf18ad19f8c4a59216
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-02-28 18:08:37 +0300

    Add first feature spec to test signing process

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature 'Signing in' do
+  let(:employee) { create(:employee) }
+
+  scenario 'Signing in with correct credentials' do
+    visit root_path
+
+    fill_in 'employee[email]', with: employee.email
+    fill_in 'employee[password]', with: employee.password
+    click_button I18n.t('views.auth.new.log_in')
+
+    expect(page).to have_content(I18n.t('devise.sessions.signed_in'))
+  end
+
+  scenario 'Signing in with incorrect credentials' do
+    visit root_path
+
+    fill_in 'employee[email]', with: 'me@example.com'
+    fill_in 'employee[password]', with: 'password'
+    click_button I18n.t('views.auth.new.log_in')
+
+    expect(page).to have_content(I18n.t('devise.failure.invalid', authentication_keys: 'Электронная почта'))
+  end
+end
