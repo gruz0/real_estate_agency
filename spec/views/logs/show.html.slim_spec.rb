0b32cc8edfa79971265bb95b2408b184d30fc9ff
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-03-17 15:06:32 +0300

    Render form params in Logs#show view

--- spec/views/logs/show.html.slim_spec.rb
+++ spec/views/logs/show.html.slim_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'logs/show', type: :view do
            controller: 'TestController',
            action: 'destroy',
            entity_id: 10,
+           params: 'Form Params',
            error_messages: 'Error Messages',
            flash_notice: 'Flash Notice')
   end
@@ -22,6 +23,7 @@ RSpec.describe 'logs/show', type: :view do
     expect(rendered).to match(/TestController/)
     expect(rendered).to match(/destroy/)
     expect(rendered).to match(/10/)
+    expect(rendered).to match(/Form Params/)
     expect(rendered).to match(/Error Messages/)
     expect(rendered).to match(/Flash Notice/)
     expect(rendered).to match(/#{I18n.l(log.created_at, format: :custom)}/)
