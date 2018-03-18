e8d2ade5e3e968c1ca886538b3a7798e2c16c515
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-03-18 16:00:18 +0300

    Update render_audited_employee_for
    If related employee was destroyed helper will return label 'Destroyed'

--- app/helpers/audits_helper.rb
+++ app/helpers/audits_helper.rb
@@ -1,6 +1,7 @@
 module AuditsHelper
   def render_audited_employee_for(audit)
     return '' if audit.user_id.blank?
+    return t('helpers.label.destroyed') unless audit.user
 
     link_to(person_fullname(audit.user), employee_path(audit.user))
   end
--- spec/helpers/audits_helper_spec.rb
+++ spec/helpers/audits_helper_spec.rb
@@ -23,5 +23,19 @@ RSpec.describe AuditsHelper, type: :helper do
           .to eq(link_to('Иванов Иван Иванович', employee_path(audit.user)))
       end
     end
+
+    context 'when employee destroyed' do
+      it 'returns label Destroyed' do
+        employee = create(:employee, last_name: 'Иванов', first_name: 'Иван', middle_name: 'Иванович')
+
+        Audited.audit_class.as_user(employee) { create(:city) }
+
+        audit = Audited::Audit.last
+
+        employee.destroy
+
+        expect(helper.render_audited_employee_for(audit)).to eq(I18n.t('helpers.label.destroyed'))
+      end
+    end
   end
 end
