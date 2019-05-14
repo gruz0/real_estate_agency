27dbccb300138fdeb3e3e8ca320e1300b438b670
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-05-14 18:31:10 +0300

    Add bulk: true to Devise migration

--- db/migrate/20180209131147_add_devise_to_employees.rb
+++ db/migrate/20180209131147_add_devise_to_employees.rb
@@ -1,6 +1,6 @@
 class AddDeviseToEmployees < ActiveRecord::Migration[5.1]
   def self.up
-    change_table :employees do |t|
+    change_table :employees, bulk: true do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
