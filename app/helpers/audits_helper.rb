79fa2760a97f4f8514aefeea65a6902813a3448e
Alexander Kadyrov <gruz0.mail@gmail.com>
2020-02-22 12:23:38 +0300

    Remove destroyed employees from audits

--- app/helpers/audits_helper.rb
+++ app/helpers/audits_helper.rb
@@ -1,7 +1,6 @@
 module AuditsHelper
   def render_audited_employee_for(audit)
-    return '' if audit.user_id.blank?
-    return t('helpers.label.destroyed') unless audit.user
+    return '' unless audit.user
 
     link_to(person_fullname(audit.user), employee_path(audit.user))
   end
